0f01c870f5b9064152f56e916f47f4f7c93d8e18
Mahfuz Rahman <mahfuzrahman517@gmail.com>
2023-08-25 22:23:12 +0600

    first commit

---
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -0,0 +1,92 @@
+import { AcademicSemester, Prisma } from '@prisma/client';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
+import prisma from '../../../shared/prisma';
+import { AcademicSemesterSearchAbleFields } from './academicSemester.contants';
+import { IAcademicSemeterFilterRequest } from './academicSemester.interface';
+
+const insertIntoDB = async (
+  academicSemesterData: AcademicSemester
+): Promise<AcademicSemester> => {
+  const result = await prisma.academicSemester.create({
+    data: academicSemesterData,
+  });
+
+  return result;
+};
+
+const getAllFromDB = async (
+  filters: IAcademicSemeterFilterRequest,
+  options: IPaginationOptions
+): Promise<IGenericResponse<AcademicSemester[]>> => {
+  const { page, limit, skip } = paginationHelpers.calculatePagination(options);
+  const { searchTerm, ...filterData } = filters;
+  console.log(options);
+  //   console.log(filtersData, filters);
+
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      OR: AcademicSemesterSearchAbleFields.map(field => ({
+        [field]: {
+          contains: searchTerm,
+          mode: 'insensitive',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map(key => ({
+        [key]: {
+          equals: (filterData as any)[key],
+        },
+      })),
+    });
+  }
+  //   console.log(filters);
+
+  const whereConditions: Prisma.AcademicSemesterWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.academicSemester.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            [options.sortBy]: options.sortOrder,
+          }
+        : {
+            createdAt: 'desc',
+          },
+  });
+  const total = await prisma.academicSemester.count();
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
+
+const getDataById = async (id: string): Promise<AcademicSemester | null> => {
+  const result = await prisma.academicSemester.findUnique({
+    where: {
+      id,
+    },
+  });
+  return result;
+};
+
+export const AcademicSemesterService = {
+  insertIntoDB,
+  getAllFromDB,
+  getDataById,
+};
